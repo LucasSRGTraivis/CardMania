cd429850a7cc880e8a7bfc54f841b7109abd7b6c
LucasSRGTraivis <lucas@traivis.io>
2026-03-09 17:26:35 +0100

    add notifications, souhaits, friends

--- components/WishlistClient.tsx
+++ components/WishlistClient.tsx
@@ -294,7 +294,7 @@ export default function WishlistClient() {
               )}
             </div>
           ) : (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {filteredWishes.map((wish) => (
                 <article
                   key={wish.id}
